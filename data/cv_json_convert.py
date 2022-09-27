23bea1af46e97b12544ed223db79607ae4d4002c
Chloe Papalazarou <cpapalazarou@sparkgeo.com>
2022-09-27 09:29:24 -0700

    crash data json created

--- data/cv_json_convert.py
+++ data/cv_json_convert.py
@@ -1,11 +1,14 @@
 import csv, json
+from re import L
 from geojson import Feature, FeatureCollection, Point
 
 #Latitude	Longitude	Location	Municipality	Crash Type	Year	Crash Count
 features = []
 with open('/Users/cpapalaz/hackathon_pgrouting/data-hackathon-routing-CJJ/data/LowerMainlandCrashes_FullData_data.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
+    reader = csv.reader(csvfile, delimiter='\t')
+    next(reader, None) 
     for Latitude, Longitude, Location, Municipality, Type, Year, Count in reader:
+        print(Latitude, Longitude)
         Latitude, Longitude = map(float, (Latitude, Longitude))
         features.append(
             Feature(
@@ -21,5 +24,5 @@ with open('/Users/cpapalaz/hackathon_pgrouting/data-hackathon-routing-CJJ/data/L
         )
 
 collection = FeatureCollection(features)
-with open("GeoObs.json", "w") as f:
+with open("crash_data.json", "w") as f:
     f.write('%s' % collection)
