a50c40573eb59b5ccb734bc4682885f0dd34a504
Joe Burkinshaw <1443875+JBurkinshaw@users.noreply.github.com>
2022-09-28 09:07:58 -0700

    Fix key

--- routing-ui/src/Map/Map.jsx
+++ routing-ui/src/Map/Map.jsx
@@ -1,14 +1,12 @@
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 
 const Map = ({routeGeoJSON}) => {
-  console.log('key', `geojson-key${routeGeoJSON.features?.length}`)
-
   return(  <MapContainer center={[49.2827, -123.1207]} zoom={11} scrollWheelZoom={true}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />``
-    {!!routeGeoJSON && <GeoJSON key={`geojson-key${routeGeoJSON.features?.length}`} data={routeGeoJSON}/>}
+    {!!routeGeoJSON && <GeoJSON key={`geojson-key${routeGeoJSON?.features?.length}`} data={routeGeoJSON}/>}
     </MapContainer>)
 }
 
